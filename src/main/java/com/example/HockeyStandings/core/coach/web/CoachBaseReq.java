0b1b1f066f744d292e3a78d0e11c25806855443b
Prithvi <prithvisinghtewatia@gmail.com>
2022-02-07 15:53:50 +0530

    Created the Coach Base Request.

---
+++ src/main/java/com/example/HockeyStandings/core/coach/web/CoachBaseReq.java
@@ -0,0 +1,40 @@
+package com.example.HockeyStandings.core.coach.web;
+
+import com.example.HockeyStandings.base.BaseRequest;
+
+import javax.validation.constraints.NotEmpty;
+
+public class CoachBaseReq extends BaseRequest {
+    @NotEmpty
+    private String name;
+    @NotEmpty
+    private int age;
+    @NotEmpty
+    private int experience;
+    private Long teamId;
+
+    public String getName(){
+        return name;
+    }
+    public void setName(String name){
+        this.name=name;
+    }
+    public int getAge(){
+        return age;
+    }
+    public void setAge(int age){
+        this.age=age;
+    }
+    public int getExperience(){
+        return experience;
+    }
+    public void setExperience(int experience){
+        this.experience=experience;
+    }
+    public Long getTeamId(){
+        return teamId;
+    }
+    public void setTeamId(Long teamId){
+        this.teamId=teamId;
+    }
+}
