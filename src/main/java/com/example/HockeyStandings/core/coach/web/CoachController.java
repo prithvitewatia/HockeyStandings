6241ad1e400cd363662ab792c60353932bb0eb79
Prithvi <prithvisinghtewatia@gmail.com>
2022-02-08 17:41:36 +0530

    Created Coach Controller.

---
+++ src/main/java/com/example/HockeyStandings/core/coach/web/CoachController.java
@@ -0,0 +1,44 @@
+package com.example.HockeyStandings.core.coach.web;
+
+import com.example.HockeyStandings.core.coach.Coach;
+import com.example.HockeyStandings.core.coach.CoachService;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+
+
+@RestController
+@RequestMapping("/coach")
+public class CoachController {
+    private final CoachService service;
+    public CoachController(CoachService service){
+        this.service=service;
+    }
+    @GetMapping("/{id}")
+    @ResponseBody
+    public CoachView getCoach(@PathVariable Long id){
+        return service.getCoach(id);
+    }
+    @GetMapping
+    @ResponseBody
+    public Page<CoachView> getAllCoaches(@PageableDefault(sort="id",direction=Sort.Direction.ASC) Pageable pageable){
+        return service.findAllCoach(pageable);
+    }
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    @ResposneBody
+    public CoachView create(@RequestBody @Valid CoachBaseReq req){
+        return service.create(req);
+    }
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteCoach(@PathVariable Long id){
+        service.delete(id);
+    }
+    @PutMapping("/{id}")
+    public CoachView updateCoach(@PathVariable(name="id") Long id,
+                                 @RequestBody @Valid CoachBaseReq req){
+        Coach coach= service.findCoachOrThrow(id);
+        return service.update(coach,req);
+    }
+}
